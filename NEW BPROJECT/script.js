a7d067878a1024c406eb07e584e119b6da3def8d
devEmkiddo <149944165+devEmkiddo@users.noreply.github.com>
2023-12-15 21:06:53 +0100

    Add files via upload

---
+++ NEW BPROJECT/script.js
@@ -0,0 +1,32 @@
+ScrollReveal({
+    reset: true,
+    distance: '5%',
+    duration: 2500,
+    delay: 400
+   });
+
+   ScrollReveal().reveal('h1', { 
+       delay: 600, 
+       origin: "left"
+   });
+   ScrollReveal().reveal('h2', { 
+       delay: 600, 
+       origin: "left"
+   });
+   ScrollReveal().reveal('h3', { 
+       delay: 600, 
+       origin: "right"
+   });
+   ScrollReveal().reveal('.home-img', { 
+       delay: 600, 
+       origin: "top"
+   });
+
+   ScrollReveal().reveal('.crd', { 
+       delay: 600, 
+       origin: "bottom"
+   });
+   ScrollReveal().reveal('.img', { 
+       delay: 600, 
+       origin: "bottom"
+   });
